c78fc470cd3eed22f468245444ef5971fe5cc10a
furtherandsun <daniel.gradin@outlook.com>
2014-02-19 03:29:07 +0100

    Forgot to add classes to repo

---
+++ SearchAndSort.UnitTest/SortTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SearchAndSort.UnitTest
+{
+    [TestClass]
+    public class SortTests
+    {
+
+        [TestMethod]
+        public void BubbleSort_IntegerArray_SortedAscending()
+        {
+            //arrange
+            var values = new int[] { 5, 1, 3, -2, -8 };
+            var sortedValues = new int[] { -8, -2, 1, 3, 5 };
+
+            //act
+            Sort.BubbleSort<int>(values);
+            
+            //assert
+            CollectionAssert.AreEqual(values, sortedValues);
+        }
+
+        [TestMethod]
+        public void BubbleSort_StringArray_SortedAscending()
+        {
+            //arrange
+            var values = new string[] { "kalle", "per", "Isa", "adam", "Anna" };
+            var sortedValues = new string[] { "adam", "Anna", "Isa", "kalle", "per" };
+
+            //act
+            Sort.BubbleSort<string>(values);
+
+            //assert
+            CollectionAssert.AreEqual(values, sortedValues);
+        }
+    }
+
+
+}
---
+++ SearchAndSort/Sort.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAndSort
+{
+    /// <summary>
+    /// Implementation of various sorting algorithms.
+    /// </summary>
+    public static class Sort
+    {
+       
+        public static void BubbleSort<T> (T[] array) where T: IComparable<T>
+        {
+            if (array == null || array.Length == 0 || array.Length == 1)
+            {
+                return;
+            }
+
+            int rightBoundary = array.Length;
+            bool hasSwapped = false;
+
+            do
+            {
+                hasSwapped = false;
+
+                for (int i = 1; i < rightBoundary; i++)
+                {
+                    if (array[i-1].CompareTo(array[i]) > 0)
+                    {
+                        Swap(array, i - 1, i);
+                        hasSwapped = true;
+                    }
+                }
+
+                rightBoundary--;
+
+            } while (hasSwapped);
+
+
+        }
+
+        public static void Swap<T> (T[] list, int pos1, int pos2)
+        {
+            if (list == null)
+            {
+                return;
+            }
+
+            try
+            {
+                T temp = list[pos1];
+                list[pos1] = list[pos2];
+                list[pos2] = temp;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;
+            }
+            
+        }
+    }
+}
